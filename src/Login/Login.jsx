c0e4c7c1ca3085458e470669f9d21e4508de83e3
shojol021 <shojol021@gmail.com>
2023-04-19 18:02:20 +0600

    signup and login form created

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h2>No Login Option currently</h2>
+        <div className='w-25 mx-auto border px-4 py-4 mt-5' >
+            <h2>Please Login</h2>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <p><small>New to Ema-John? Signup <Link to='/signup' className='btn btn-link'>here</Link></small></p>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
--- src/assets/Signup/Signup.jsx
+++ src/assets/Signup/Signup.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
-import './signup.css';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
     return (
-        <div id='form-container'>
-            <form action="">
-                <div id='input-container'>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="emain" id="email" />
-                </div>
-                <div id='input-container'>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="emain" id="password" />
-                </div>
-            </form>
+        <div className='w-25 mx-auto border px-4 py-4 mt-5' >
+            <h2>Please sign up</h2>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Name address</Form.Label>
+                    <Form.Control type="text" name='name' placeholder="Enter name" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <p><small>Already have an account? Login <Link to='/login' className='btn btn-link'>here</Link></small></p>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 };
